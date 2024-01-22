9a583c5726bdb3ac0179f4942ddb8fe4d7e7e470
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-21 19:20:58 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Author: Magaly Rodriguez
+ * Course: COMP-003A
+ * Purpose: Code for assignment 1
+*/
+
+namespace COMP003Aassignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I like to paint");
+            Console.WriteLine("2. I like to draw on paper and pencil");
+            Console.WriteLine("3. I like to draw on my iPad");
+            Console.WriteLine("4. I own 5 cats");
+            Console.WriteLine("5. My favorite cat is Paca");
+            Console.WriteLine("6. My favorite colors are orange and yellow");
+            Console.WriteLine("7. My hair is brown");
+            Console.WriteLine("8. My eyes are brown");
+            Console.WriteLine("9. I was born in the United States");
+            Console.WriteLine("10. My favorite food is sushi");
+      
+        }
+    }
+}
+
